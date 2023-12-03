876e5e7f2e377905ec6dba4f06a74669b87cf029
mariatveen <mariatveen@gmail.com>
2023-12-03 14:18:56 +0000

    k

--- Git.R
+++ Git.R
@@ -1,11 +1,8 @@
 library(ggplot2) jjj #lålå #j #nej
 
-#ejw
-<<<<<<< HEAD
-#M
-=======
+#ejw#M
+
 dd#Mari23Wmjjas
 #jojo
 #shit fd+ju er go 
->>>>>>> d58583734c0a0945a67fde9fb32111df769bb158
 ##shit dfffu f,er go #
